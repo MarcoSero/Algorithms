9f7e39f062a37a165b10b6abe800d479f74289d4
Marco Sero <marco@marcosero.com>
2014-06-15 15:12:50 +0100

    Initial Commit

---
+++ Algorithms/Algorithms.h
@@ -0,0 +1,19 @@
+//
+//  Algorithms.h
+//  Algorithms
+//
+//  Created by Marco Sero on 15/06/14.
+//  Copyright (c) 2014 Marco Sero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Algorithms.
+FOUNDATION_EXPORT double AlgorithmsVersionNumber;
+
+//! Project version string for Algorithms.
+FOUNDATION_EXPORT const unsigned char AlgorithmsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Algorithms/PublicHeader.h>
+
+
